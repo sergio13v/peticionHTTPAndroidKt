c9258bb4472d48e9088e8c9e5f2839ea6c8d6cff
sergio13v <56689687+sergio13v@users.noreply.github.com>
2019-12-19 15:34:11 +0100

    Initial commit

---
+++ app/src/main/java/com/example/sendsimplerequest/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.sendsimplerequest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+// Creamos una función que llama a la clase, otra función.
+
+
+   fun sendJson (view: View){
+        val textView = findViewById<TextView>(R.id.textView)
+// ...
+
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "http://192.168.1.102:3000/coches"
+
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                // Display the first 500 characters of the response string.
+                textView.text = "Response is: ${response.substring(0, 500)}"
+            },
+            Response.ErrorListener { textView.text = "That didn't work!" })
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest)
+    }
+}
